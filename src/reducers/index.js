27ec6052c24c09ddf4369682eb6cd44def01ee12
nskij <n.skij0123@gmail.com>
2019-07-02 22:08:00 +0900

    delete lodash

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,9 @@
-import _ from 'lodash'
 import { GET_QIITA_STOCKS } from '../actions/getstock'
 
 export default (items = {}, action) => {
   switch (action.type){
     case GET_QIITA_STOCKS:
-      return _.map(action.response)
+      return action.response
     default: 
       return items
   }
